304623257d51b90cfc3749158a30b76d542949ae
vicesuarez <vice_suarez@yahoo.com.ar>
2016-07-29 20:17:00 -0300

    Unmark tokens.

--- cleartk-ruta/src/main/java/tesis/clear_tk/XMIReaderTest.java
+++ cleartk-ruta/src/main/java/tesis/clear_tk/XMIReaderTest.java
@@ -1,15 +1,23 @@
 package tesis.clear_tk;
 
 import java.io.File;
-import java.nio.file.Files;
 import java.util.Collection;
 
+import opennlp.uima.util.AnnotationComboIterator;
+import opennlp.uima.util.AnnotationIteratorPair;
+import opennlp.uima.util.AnnotatorUtil;
+import opennlp.uima.util.UimaUtil;
+
 import org.apache.uima.UIMAFramework;
+import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_engine.AnalysisEngine;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.TypeSystem;
 import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.fit.factory.TypeSystemDescriptionFactory;
+import org.apache.uima.fit.factory.UimaContextFactory;
 import org.apache.uima.fit.util.CasIOUtil;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
@@ -17,14 +25,19 @@ import org.apache.uima.resource.ResourceManager;
 import org.apache.uima.resource.ResourceSpecifier;
 import org.apache.uima.resource.metadata.TypeSystemDescription;
 import org.apache.uima.ruta.engine.RutaEngine;
+import org.apache.uima.util.TypeSystemUtil;
 import org.apache.uima.util.XMLInputSource;
+import org.cleartk.ml.feature.extractor.CleartkExtractor;
+import org.cleartk.token.type.Token;
 
-import uima.ruta.example.TestPerformance.tokenTime;
+import uima.ruta.example.TestPerformance.PerformanceSentence;
+import uima.ruta.example.TestPerformance.TokenConstraint;
+import uima.ruta.example.TestPerformance.TokenTime;
 
 public class XMIReaderTest {
 
-	private static final String XMI_INPUT_FILE_PATH = "input/supplspec_v1.htm.txt.xmi";
-	private static final String XMI_OUTPUT_FILE_PATH = "output/supplspec_v1.htm.txt.xmi";
+	private static final String XMI_INPUT_FILE_PATH = "input/ejemplos performance.txt.xmi";
+	private static final String XMI_OUTPUT_FILE_PATH = "output/ejemplos performance.txt.xmi";
 
 	private static final String PERFORMANCE_ENGINE_PATH = "descriptor/uima/ruta/example/TestPerformanceEngine.xml";
 	private static final String PERFORMANCE_SCRIPT_PATH = "script/uima/ruta/example/TestPerformance.ruta";
@@ -69,12 +82,30 @@ public class XMIReaderTest {
 			ae.reconfigure();
 			ae.process(cas);
 		}
-		Collection<tokenTime> tokens = JCasUtil.select(cas.getJCas(),
-				tokenTime.class);
-		for (tokenTime token : tokens) {
-			System.out.println(token);
+		Collection<TokenConstraint> tokens = JCasUtil.select(cas.getJCas(),
+				TokenConstraint.class);
+		for (TokenConstraint token : tokens) {
 			System.out.println(token.getCoveredText());
+			System.out.println(token);
 		}
+		
+		UimaContext context = UimaContextFactory.createUimaContext(null);
+		
+		// sentence type
+	    Type sentenceType = AnnotatorUtil.getRequiredTypeParameter(context, cas.getTypeSystem(),
+	        UimaUtil.SENTENCE_TYPE_PARAMETER);
+
+	    // token type
+	    Type tokenType = AnnotatorUtil.getRequiredTypeParameter(context, cas.getTypeSystem(),
+	        UimaUtil.TOKEN_TYPE_PARAMETER);
+		
+		final AnnotationComboIterator comboIterator = new AnnotationComboIterator(cas,
+		        sentenceType, tokenType);
+		
+		for (AnnotationIteratorPair annotationIteratorPair : comboIterator) {
+		      System.out.println(annotationIteratorPair);
+		}
+		
 	}
 
 }
