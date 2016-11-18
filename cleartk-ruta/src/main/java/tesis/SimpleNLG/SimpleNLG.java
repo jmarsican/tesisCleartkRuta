b135f29a6251555859097e004cb937aa1079849c
Javier Marsicano <jmarsican@gmail.com>
2016-11-18 20:23:40 -0300

    simple NLG POC class

--- cleartk-ruta/src/main/java/tesis/SimpleNLG/SimpleNLG.java
+++ cleartk-ruta/src/main/java/tesis/SimpleNLG/SimpleNLG.java
@@ -4,6 +4,8 @@ import simplenlg.features.Feature;
 import simplenlg.features.Tense;
 import simplenlg.framework.NLGFactory;
 import simplenlg.lexicon.Lexicon;
+import simplenlg.phrasespec.NPPhraseSpec;
+import simplenlg.phrasespec.PPPhraseSpec;
 import simplenlg.phrasespec.SPhraseSpec;
 import simplenlg.realiser.english.Realiser;
 
@@ -14,10 +16,17 @@ public class SimpleNLG {
 		NLGFactory factory = new NLGFactory(lexicon);
 		SPhraseSpec phrase = new SPhraseSpec(factory);
 		Realiser realiser = new Realiser(lexicon);
-
-		phrase.setSubject(factory.createNounPhrase("my dog"));
-		phrase.setVerb(factory.createVerbPhrase("chasing"));
-		phrase.setObject(factory.createNounPhrase("George"));
+		
+		NPPhraseSpec complement = factory.createNounPhrase("user");
+		complement.setDeterminer("the");
+		PPPhraseSpec prepPhrase = factory.createPrepositionPhrase();
+		prepPhrase.setPreposition("on");
+		prepPhrase.setComplement(complement);
+				
+		
+		phrase.setVerb(factory.createVerbPhrase("display"));
+		phrase.setObject(factory.createNounPhrase("the alarm"));
+		phrase.addModifier(prepPhrase);
 		
 		phrase.setFeature(Feature.TENSE, Tense.PRESENT);
 		
