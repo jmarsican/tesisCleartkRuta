974543940cb28feabb48bf21af74f15104b30fca
vicesuarez <andresvicen@gmail.com>
2016-11-30 20:52:22 -0300

    Adding compounds to phrases generation.

--- cleartk-ruta/src/main/java/tesis/extractors/NounModExtractor.java
+++ cleartk-ruta/src/main/java/tesis/extractors/NounModExtractor.java
@@ -1,14 +1,15 @@
 package tesis.extractors;
 
-import java.util.Optional;
+import java.util.stream.Stream;
 
 import simplenlg.features.Feature;
 import simplenlg.features.Tense;
 import simplenlg.framework.NLGFactory;
+import simplenlg.framework.PhraseElement;
 import simplenlg.phrasespec.NPPhraseSpec;
 import simplenlg.phrasespec.PPPhraseSpec;
 import simplenlg.phrasespec.SPhraseSpec;
-import edu.stanford.nlp.ling.CoreLabel;
+import simplenlg.phrasespec.VPPhraseSpec;
 import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.semgraph.SemanticGraph;
 import edu.stanford.nlp.semgraph.SemanticGraphEdge;
@@ -22,26 +23,35 @@ public class NounModExtractor implements IPhraseExtractor {
 		mFactory = nlgFactory;
 	}
 	
-	private IndexedWord getDependent(SemanticGraph graph, IndexedWord node, String relationShortName) {
-		Optional<IndexedWord> indexedWord = 
-				graph.getOutEdgesSorted(node).stream()
-				.filter(e -> relationShortName.equals(e.getRelation().getShortName()))
-				.findFirst().map(e -> e.getDependent());
-		return indexedWord.orElse(EMPTY_INDEXED_WORD);
+	private Stream<SemanticGraphEdge> getOutEdgesSorted(SemanticGraph graph, IndexedWord node, String relationShortName){
+		return graph.getOutEdgesSorted(node).stream()
+				.filter(e -> relationShortName.equals(e.getRelation().getShortName()));
 	}
-
+	
+	private IndexedWord getDependent(SemanticGraph graph, IndexedWord node, String relationShortName) {
+		return getOutEdgesSorted(graph, node, relationShortName).findFirst()
+				.map(e -> e.getDependent())
+				.orElse(EMPTY_INDEXED_WORD);
+	}
+	
+	private void addCompounds(SemanticGraph graph, IndexedWord node, PhraseElement phrase) {
+		getOutEdgesSorted(graph, node, "compound")
+			.forEach(edge -> phrase.addPreModifier(edge.getDependent().originalText()));
+	}
+	
 	@Override
 	public SPhraseSpec assemble(SemanticGraph graph, SemanticGraphEdge edge) {
 			IndexedWord indexedVerb = edge.getGovernor(); 
+			IndexedWord indexedObject = edge.getDependent();
 			IndexedWord indexedComplement = getDependent(graph, indexedVerb, "nmod");
 			IndexedWord indexedPreposition = getDependent(graph, indexedComplement, "case");
 			IndexedWord indexedDeterminer = getDependent(graph, indexedComplement, "det");
 			
-			String verb = indexedVerb.originalText(); // update
-			String object = edge.getDependent().originalText(); // alarm
-			String complement = indexedComplement.originalText(); // user
-			String preposition = indexedPreposition.originalText(); // on
-			String complementDeterminer = indexedDeterminer.originalText(); // the
+			String verb = indexedVerb.originalText();
+			String object = indexedObject.originalText();
+			String complement = indexedComplement.originalText();
+			String preposition = indexedPreposition.originalText();
+			String complementDeterminer = indexedDeterminer.originalText();
 			
 			SPhraseSpec phrase = new SPhraseSpec(mFactory);
 			
@@ -50,10 +60,18 @@ public class NounModExtractor implements IPhraseExtractor {
 			PPPhraseSpec nlpModifier = mFactory.createPrepositionPhrase();
 			nlpModifier.setPreposition(preposition);
 			nlpModifier.setComplement(nlpComplement);
-			
-			phrase.setVerb(mFactory.createVerbPhrase(verb));
-			phrase.setObject(mFactory.createNounPhrase(object));
+
+			VPPhraseSpec verbPhrase = mFactory.createVerbPhrase(verb);
+			boolean negated = getDependent(graph, indexedVerb, "neg") != EMPTY_INDEXED_WORD;
+			verbPhrase.setFeature(Feature.NEGATED, negated);
+			phrase.setVerb(verbPhrase);
+			addCompounds(graph, indexedVerb, verbPhrase);
+
+			NPPhraseSpec nounPhrase = mFactory.createNounPhrase(object);
+			phrase.setObject(nounPhrase);
+			addCompounds(graph, indexedObject, nounPhrase);
 			phrase.addModifier(nlpModifier);
+			addCompounds(graph, indexedComplement, nlpComplement);
 
 			phrase.setFeature(Feature.TENSE, Tense.PRESENT);
 		return phrase;
