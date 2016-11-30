130069cdb60ef76e190b4aa7f44de19092adcced
Javier Marsicano <jmarsican@gmail.com>
2016-11-30 19:27:53 -0300

    DObj extractor

--- cleartk-ruta/src/main/java/tesis/extractors/DirectObjectExtractor.java
+++ cleartk-ruta/src/main/java/tesis/extractors/DirectObjectExtractor.java
@@ -2,6 +2,7 @@ package tesis.extractors;
 
 import simplenlg.framework.NLGFactory;
 import simplenlg.phrasespec.SPhraseSpec;
+import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.semgraph.SemanticGraph;
 import edu.stanford.nlp.semgraph.SemanticGraphEdge;
 
@@ -11,7 +12,17 @@ public class DirectObjectExtractor extends PhraseExtractor {
 
 	@Override
 	public SPhraseSpec assemble(SemanticGraph graph, SemanticGraphEdge edge) {
-		// TODO Auto-generated method stub
+		IndexedWord indexedVerb = edge.getGovernor(); 
+		IndexedWord indexedComplement = getDependent(graph, indexedVerb, "nmod");
+		IndexedWord indexedModifier = getDependent(graph, edge.getDependent(), "amod");
+		IndexedWord indexedDeterminer = getDependent(graph, edge.getDependent(), "det");
+		
+		String verb = indexedVerb.originalText(); // update
+		String object = edge.getDependent().originalText(); // alarm
+		String complement = indexedComplement.originalText(); // user
+		String modifier = indexedModifier.originalText(); 
+		String complementDeterminer = indexedDeterminer.originalText(); // the
+		
 		return null;
 	}
 
