c6a7e6d3dcbe69c3bb81c2dba799c3d1940e29f2
Javier Marsicano <jmarsican@gmail.com>
2017-05-08 20:05:10 -0300

    scenario generation - combinatoria

--- modeling/edu.exa.unicen.qascenario.ui/src/edu/exa/unicen/qascenario/ui/popup/actions/ScenarioGenerator.java
+++ modeling/edu.exa.unicen.qascenario.ui/src/edu/exa/unicen/qascenario/ui/popup/actions/ScenarioGenerator.java
@@ -1,30 +1,45 @@
 package edu.exa.unicen.qascenario.ui.popup.actions;
 
+import java.util.Map;
+
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+import org.eclipse.jface.viewers.ListViewer;
 
 import scenario.ScenarioPackage;
 import scenario.impl.GeneralScenarioImpl;
 import scenario.impl.ScenarioFactoryImpl;
 
-public class ScenarioGenerator {
+public abstract class ScenarioGenerator {
+	protected URI mUri;
+	
+	public ScenarioGenerator(URI uri) {
+		mUri = uri;
+	}
 
-  public static GeneralScenarioImpl createNewScenarioInstance() {
-    GeneralScenarioImpl scenario =
-        (GeneralScenarioImpl) ScenarioFactoryImpl.eINSTANCE.createGeneralScenario();
+	public GeneralScenarioImpl createNewScenarioInstance() {
+		GeneralScenarioImpl scenario = (GeneralScenarioImpl) ScenarioFactoryImpl.eINSTANCE
+				.createGeneralScenario();
 
-    ResourceSet resourceSet = new ResourceSetImpl();
-    resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap()
-        .put(Resource.Factory.Registry.DEFAULT_EXTENSION, new XMIResourceFactoryImpl());
-    resourceSet.getPackageRegistry().put(ScenarioPackage.eNS_URI, ScenarioPackage.eINSTANCE);
+		ResourceSet resourceSet = new ResourceSetImpl();
+		resourceSet
+				.getResourceFactoryRegistry()
+				.getExtensionToFactoryMap()
+				.put(Resource.Factory.Registry.DEFAULT_EXTENSION,
+						new XMIResourceFactoryImpl());
+		resourceSet.getPackageRegistry().put(ScenarioPackage.eNS_URI,
+				ScenarioPackage.eINSTANCE);
 
-    Resource resource = resourceSet.createResource(URI.createURI("http:///My.scenario"));
-    resource.getContents().add(scenario);
+		Resource resource = resourceSet.createResource(URI
+				.createURI("http:///My.scenario"));
+		resource.getContents().add(scenario);
 
-    return scenario;
-  }
+		return scenario;
+	}
 
+	public abstract void generate(Map<EStructuralFeature, ListViewer> map);
 }
